04b2eef5c7cfd204fed9d9ca6f3412340286e5d8
Tish Demsas <at.demsas@gmail.com>
2016-06-06 09:10:26 -0700

    updated form fields

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,13 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.limit(3)
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.where("name Like ?", "%#{search_term}%")
+      # return filtered result
+    else
+      @products = Product.limit(3)
+    end
   end
 
   # GET /products/1
