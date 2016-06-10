ff6e3986557c60f2613d0a2e7d772cfd20a5ab0e
Tish Demsas <at.demsas@gmail.com>
2016-06-10 10:58:21 -0700

    added mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+
+ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
   def about
   end
 
@@ -8,4 +10,14 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.limit(3)
   end
+  def thank_you
+  		@name = params[:name]
+  		@email = params[:email]
+  		@message = params[:message]
+  		ActionMailer::Base.mail(:from => @email,
+      		:to => 'at.demsas@gmail.com',
+      		:subject => "A new contact form message from #{@name}",
+      		:body => @message).deliver_now
+  	end
+
 end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,11 @@
+class ApplicationMailer < ActionMailer::Base
+ 	def thank_you
+  		@name = params[:name]
+  		@email = params[:email]
+  		@message = params[:message]
+  		ActionMailer::Base.mail(:from => @email,
+      		:to => 'at.demsas@gmail.com',
+      		:subject => "A new contact form message from #{@name}",
+      		:body => @message).deliver_now
+	end
+end
